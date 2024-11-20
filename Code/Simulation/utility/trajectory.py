a454c863b8e27bd3aa273e485ff1b7920eba5207
Lars Bunszey <bunszey@gmail.com>
2024-11-20 23:57:19 +0100

    added trajectory planner

--- Code/Simulation/utility/trajectory.py
+++ Code/Simulation/utility/trajectory.py
@@ -1,35 +1,25 @@
 import numpy as np
 
 
-def min_jerk_traj( t: float, ti: float, tf: float, pi: float, pf: float, D: float ):
-    """
-        Returning the 1D position and velocity data at time t of the minimum-jerk-trajectory ( current time )
-        Time should start at t = 0
-        Note that the minimum-jerk-trajectory remains at the initial (respectively, final) posture before (after) the movement.
-        Arguments
-        ---------
-            [1] t : current time
-            [2] ti: start of the movement
-            [3] tf: end   of the movement
-            [4] pi: initial ( reference ) posture
-            [5] pf: final   ( reference ) posture
-            [6]  D: duration
-    """
+class MinJerkTrajectoryPlanner:
+    def __init__(self, initial_positions, final_positions, duration):
+        self.q_i = np.array(initial_positions)  # Initial joint positions (6 joints)
+        self.q_f = np.array(final_positions)    # Final joint positions (6 joints)
+        self.D = duration                       # Duration of the trajectory
+        self.delta_q = self.q_f - self.q_i      # Change in joint positions
 
-    assert  t >=  0 and ti >= 0 and tf >= 0 and D >= 0
-    assert tf >= ti
-
-    if   t <= ti:
-        pos = pi
-        vel = 0
-
-    elif ti < t <= tf:
-        tau = ( t - ti ) / D                                                # Normalized time
-        pos =     pi + ( pf - pi ) * ( 10. * tau ** 3 - 15. * tau ** 4 +  6. * tau ** 5 )
-        vel = 1. / D * ( pf - pi ) * ( 30. * tau ** 2 - 60. * tau ** 3 + 30. * tau ** 4 )
-
-    else:
-        pos = pf
-        vel = 0
-
-    return pos, vel
+    def get_desired_position(self, t):
+        if t >= self.D:
+            return self.q_f
+        tau = t / self.D
+        scaling_factor = 10 * tau**3 - 15 * tau**4 + 6 * tau**5
+        q_d = self.q_i + self.delta_q * scaling_factor
+        return q_d
+    
+    def get_desired_velocity(self, t):
+        if t >= self.D:
+            return np.zeros(6)
+        tau = t / self.D
+        scaling_factor = 30 * tau**2 - 60 * tau**3 + 30 * tau**4
+        dq_d = self.delta_q * scaling_factor / self.D
+        return dq_d
